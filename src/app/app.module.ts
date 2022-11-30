9dce0e5410a712273573fb00d2c45d4caba1f3a7
“IwaoM” <iwao27@gmail.com>
2022-11-30 19:33:02 +0100

    Added icons & components - Added Timeline page & navbar components - Added the logo and navigation icons in the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChirpComponent } from './chirp/chirp.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChirpComponent
+    ChirpComponent,
+    TimelineComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
